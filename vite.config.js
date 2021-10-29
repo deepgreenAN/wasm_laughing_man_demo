0ab3aaf2e9cadd754754fb7a264d65835d5c61fd
deepgreenAN <asami73dgreen63@gmail.com>
2021-10-29 17:57:24 +0900

    for deploy change

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,27 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import devcert from 'devcert'
 
 //https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [svelte()],
-//   server: {
-//     host: true
-//   }
-// })
-
-export default defineConfig(async () => {
-  const { key, cert } = await devcert.certificateFor('localhost');
-  //const { key, cert } = await devcert.certificateFor('192.168.1.3');
-
-  return {
-    root: './',
-    plugins:  [svelte()],
-    server: {
-      open: true,
-      https: {
-        key,
-        cert,
-      },
-      host: true
-    },
+export default defineConfig({
+  plugins: [svelte()],
+  server: {
+    host: true
   }
 })
+
+// export default defineConfig(async () => {
+//   const { key, cert } = await devcert.certificateFor('localhost');
+//   //const { key, cert } = await devcert.certificateFor('192.168.1.3');
+
+//   return {
+//     root: './',
+//     plugins:  [svelte()],
+//     server: {
+//       open: true,
+//       https: {
+//         key,
+//         cert,
+//       },
+//       host: true
+//     },
+//   }
+// })
